e6953ca405834c0b5fe9844b5a094eb24dd99920
KuzminaSofia <sivanstiven@gmail.com>
2021-10-07 12:55:04 +0300

    void сохранение в файл, пока не работает. знать бы почему

--- Kuzminalab11/Kuzminalab11.cpp
+++ Kuzminalab11/Kuzminalab11.cpp
@@ -3,13 +3,14 @@
 
 #include "pch.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
 struct Pipe
 {
     int id;
     int d; 
     int l;
-    bool r;
+    bool r;  // была bool
  
 };
 
@@ -17,33 +18,35 @@ struct CS
 {
     int id;
     string name;
-    int col_cex; // количество цехов
-    int col_work; // количество в работе 
-    double effic; // эффективность
+    int col_cex;                                                                               // количество цехов
+    int col_work;                                                                              // количество в работе 
+    double effic;                                                                              // эффективность
 };
 
 
 void PrintePipe(Pipe& p)
 {
-    cout << " Pipe ID number: " << p.id << "  Diameter: " << p.d << " mm " << " Lenght: " << p.l << " km \n" ;
-    cout << " Pipe under repair: " << p.r <<endl;
+    cout << " Труба под номером: " << p.id << "  Диаметр: " << p.d << " мм " << " Длина: " << p.l << " км\n" ;
+    cout << " Труба в ремонте: " << p.r <<endl;
 }
+
 void PrinteCS(CS& station)
 {
-    cout << " CS ID number: " << station.id << " Name: " << station.name << " Number of shops: " << station.col_cex << " Work: " << station.col_work;
+    cout << " Номер кс: " << station.id << " Имя: " << station.name << " Количество цехов: " << station.col_cex << " Количество цехов в работе: " << station.col_work;
 }
+
 Pipe AddPipe()
 {
     Pipe p;
     p.id = 0;
-    cout << "Enter the diameter ";
-    cin >> p.d;
+    cout << "Введите диаметр ";
+    cin >> p.d; 
     while (p.d < 400 || p.d > 2000)
     {
         cout << "ВВедите число из промежутка [400;2000] mm ";
         cin >> p.d;
     }
-    cout << "Enter the lenght ";
+    cout << "Введите длину ";
     cin >> p.l;
     while (p.l < 10 || p.l > 100)
     {
@@ -59,33 +62,40 @@ CS AddCS()
 {
     CS station;
     station.id = 0;
-    cout << "Enter the name ";
+    cout << "Введите имя кс ";
     cin >> station.name;
-    cout << "How many shops are there? ";                  // сколько цехов в станции?
+    cout << "Сколько цехов в станции? ";                                                  // сколько цехов в станции?
     cin >> station.col_cex;
-    cout << "How many shops do we have in the job? ";      // сколько цехов в работе?
+    cout << "Сколько цехов в работе? ";                                                   // сколько цехов в работе?
     cin >> station.col_work;
 
     while (station.col_work > station.col_cex)
     {
-        cout << "Количество рабочих больше чем всего цехов";
+        cout << "Количество рабочих больше чем всего цехов. Пожалуйста, введите новое значение: ";
         cin >> station.col_work;
     }
     return station;
 
-}
-Pipe editpipe(Pipe& p)                                    //редактор трубы
+}  
+Pipe editpipe(Pipe& p)                                                                   //редактор трубы
 {
-    if (p.l > 1) //существует
+    if (p.l > 1)                                                                         //существует
     {
-        cout << "Work 0";                            //не работает ничего кроме 1 и 0, вылетает прога
+        cout << "Если труба в работе нажмите - 0, если в ремонте - 1 \n";               //не работает ничего кроме 1 и 0, вылетает прога
         cin >> p.r;
+      //  while (p.r != 0 || p.r != 1)
+      //  {
+      //      cout << "Введите 1 или 0 ";
+      //      cin >> p.r;
+      // }
+      
+
     }
     else cout << "Добавьте трубу";
         return p;
 }
 
-CS editcs(CS& station)                                   //редактор компрессорной станции
+CS editcs(CS& station)                                                                  //редактор компрессорной станции
 {
     if (station.col_cex > 0)
     {
@@ -97,16 +107,49 @@ CS editcs(CS& station)                                   //редактор компрессорно
 
 }
 
+void Save(Pipe p, CS station)                                                          //сохранение в файл
+{
+    ofstream file;
+    file.open("vyvod.txt");
+    if (file.good())
+    {
+        if (p.l > 0)
+        {
+            file << "Pipe: \n";
+            file << p.id << "\n";
+            file << p.d << "\n";
+            file << p.l << "\n";
+            file << p.r << "\n";
+        }
+        if (station.name != "")
+        {
+            file << "CS: \n";
+            file << station.id << "\n";
+            file << station.name << "\n";
+            file << station.col_cex << "\n";
+            file << station.col_work << "\n";
+        }
+        file.close();
+        cout << "Сохранено\n";
+    }
+}
+
+
+
 int main()
 {
 
-    int m; // создали меню
+    int m;                                                                             // создали меню
 
     Pipe p = {};
     CS station = {};
 
+    {
+        setlocale(0, "");                                                              //русский язык  
+    }
+
     for (;;) {
-        cout << "\n 1. Exit\n 2. Add pipe\n 3. Add CS\n 4. All objects\n 5. EditPipe\n 6. EditCS\n 7. Save\n 8. Dowland\n";
+        cout << "\n 1. Выход\n 2. Добавить трубу\n 3. Добавить кс\n 4. Показать все объекты\n 5. Редактировать трубу\n 6. Редактировать кс\n 7. Сохранить\n 8. Загрузить\n";
         cin >> m;
         switch (m)
         {
@@ -141,11 +184,16 @@ int main()
             system("cls");
             editcs(station);
             break;
+        case 7:
+            system("cls");
+            Save(p, station);
+            break;
+
 
         }
 
     }
-
+    return 0;
     }
 
 
